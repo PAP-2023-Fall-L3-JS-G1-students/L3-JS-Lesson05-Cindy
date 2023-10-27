6ea121452caa423bb23a6ba5ae55f453ae47e5fe
Cindy Nguyen <CindyNguyen@computingforall.org>
2023-10-26 17:21:46 -0700

    finished in class assignment

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 //Exercise 3.2
 
+let form = document.querySelector('#form');
 function generate(event) {
     /*The line below prevents the form from trying to 
     send data to a server and refreshing the page*/
     event.preventDefault();
 
     //Exercise 3.3 
+    let invitee = document.querySelector('#invitee').value;
+    let date = document.querySelector('#date').value;
+    let sender = document.querySelector('#sender').value;
+    let button = document.querySelector('#button').value;
     
-    
+    console.log(invitee, date, sender, button);
+
+    // solution 1 
+    document.querySelector('#party-invitee').textContent = invitee;
+ 
+    document.querySelector('#party-date').textContent = date;
+   
+    document.querySelector('#party-host').textContent = sender;
+  
     //Exercise 3.4
 
 
@@ -17,3 +30,5 @@ function generate(event) {
 }
 
 //Exercise 3.5
+
+form.addEventListener("submit", generate);
